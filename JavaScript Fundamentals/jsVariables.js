f73c17f387f40f516934018c651d0d4a52f77761
Ashwin <187102172+integer-datatype@users.noreply.github.com>
2025-09-25 00:34:18 +0530

    add JS variables

---
+++ JavaScript Fundamentals/jsVariables.js
@@ -0,0 +1,24 @@
+let message;       // undefined
+message = 'hello'; // initialized  the variable with a litral string 'hello'
+
+let name = 'Bob';  // declare and initialize a variable at the same time
+
+
+let anotherName = "Sam",
+    age = 98
+
+let anotherMessage;
+console.log(anotherMessage);    // undefined
+
+
+// console.log(counter);   // ReferenceError: counter is not defined.
+
+
+// constant variable 
+const workday = 5
+
+// workday = 2     // TypeError: Assignment to constant variable.
+
+const PORT = 3000;
+// constant names in uppercase --> This is community convention, not a rule.
+
