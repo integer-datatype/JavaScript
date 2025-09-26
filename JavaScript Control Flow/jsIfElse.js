2e90a4fd132947da35a2c428a68d4b50f3156afe
Ashwin <187102172+integer-datatype@users.noreply.github.com>
2025-09-26 18:03:32 +0530

    add JS If-Else

---
+++ JavaScript Control Flow/jsIfElse.js
@@ -0,0 +1,23 @@
+// syntax
+// if (condition) {
+//     // statement executes if the condition is true
+// } else {
+//     // statement executes when the condition is false
+// }
+
+let age = 16;
+if (age >= 18) {
+    console.log('You can sign up');
+} else {
+    console.log('You must be at least 18 to sign up.');
+}
+// You must be at least 18 to sign up.
+
+let country = 'USA';
+
+if (age >= 16 && country == 'USA') {
+    console.log('You can get a driving license.');
+} else {
+    console.log('You are not eligible to get a driving license.');
+}
+// You can get a driving license.
